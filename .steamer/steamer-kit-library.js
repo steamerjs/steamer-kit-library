0fa97a7a5586e8f6d844103b8b1007691d5dae74
lcxfs1991 <lcxfs1991@gmail.com>
2018-06-12 09:26:18 +0800

    fix steamer-kit-library.js

--- .steamer/steamer-kit-library.js
+++ .steamer/steamer-kit-library.js
@@ -1,3 +1,15 @@
-'use strict';
-
-console.log(123);
+module.exports = {
+    files: [
+        'config',
+        'src',
+        'test',
+        'tools',
+        '.babelrc',
+        '.editorconfig',
+        '.eslintrc.js',
+        '.gitignore',
+        '.npmignore',
+        'README.md'
+    ],
+    options: []
+};
